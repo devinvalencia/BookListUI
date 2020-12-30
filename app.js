c3a25ef973e9af2c23a054f30afafcc4b192618f
devinvalencia <37150528+devinvalencia@users.noreply.github.com>
2020-12-30 13:18:51 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// Book Constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+// UI Constructor
+function UI() {}
+
+// Event Listener
+document.getElementById('book-form').addEventListener('submit', function(e){
+  // Get form values
+  const title = document.getElementById('title').value,
+        author = document.getElementById('author').value,
+        isbn = document.getElementById('isbn').value
+
+  // Instantiate Book
+  const book = new Book(title, author, isbn);
+
+  e.preventDefault();
+});
