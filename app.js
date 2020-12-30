0f433042b2e546a62d46888059dbb7b47852b94f
devinvalencia <37150528+devinvalencia@users.noreply.github.com>
2020-12-30 13:30:44 -0500

    Can add books now

--- app.js
+++ app.js
@@ -8,6 +8,25 @@ function Book(title, author, isbn) {
 // UI Constructor
 function UI() {}
 
+// UI Prototype, add book to list
+UI.prototype.addBookToList = function(book) {
+  let list = document.getElementById('book-list');
+  let row = document.createElement('tr');
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="" class="delete">X</a></td>`
+    list.appendChild(row);
+}
+
+// UI Prototype, clear fields
+UI.prototype.clearFields = function() {
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('isbn').value = '';
+}
+
 // Event Listener
 document.getElementById('book-form').addEventListener('submit', function(e){
   // Get form values
@@ -17,6 +36,13 @@ document.getElementById('book-form').addEventListener('submit', function(e){
 
   // Instantiate Book
   const book = new Book(title, author, isbn);
+
+  // Instaniate UI
+  const ui = new UI();
+  
+  // Add book to list
+  ui.addBookToList(book);
+  ui.clearFields();
 
   e.preventDefault();
 });
