6dc7f38ec40af7a4486a0cd8bf43cc483488350e
devinvalencia <37150528+devinvalencia@users.noreply.github.com>
2021-01-01 23:13:50 -0500

    Added local storage

--- appes6.js
+++ appes6.js
@@ -49,18 +49,62 @@ class UI {
   }
 }
 
+class Store {
+
+  static getBooks(){
+    let books;
+    if(localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+
+    return books;
+  }
+
+  static displayBooks(){
+    const books = Store.getBooks();
+
+    books.forEach(function(book){
+      const ui = new UI();
+      ui.addBooktoList(book);
+    });
+  }
+
+  static addBook(book){
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(isbn){
+    const books = Store.getBooks();
+
+    books.forEach(function(book, index){
+        if(book.isbn == isbn) {
+          books.splice(index, 1);
+        }
+    });
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+// DOM load event
+document.addEventListener('DOMContentLoaded',Store.displayBooks());
+
 // Event Listener for add book
 document.getElementById('book-form').addEventListener('submit', function(e){
   // Get form values
   const title = document.getElementById('title').value,
         author = document.getElementById('author').value,
         isbn = document.getElementById('isbn').value
-  // Instantiate Book
+
   const book = new Book(title, author, isbn);
-  // Instaniate UI
   const ui = new UI();
-  // Add book to list
+
   ui.addBooktoList(book);
+  Store.addBook(book);
   ui.clearFields();
   e.preventDefault();
 });
@@ -70,5 +114,6 @@ document.getElementById('book-form').addEventListener('submit', function(e){
 document.getElementById('book-list').addEventListener('click', function(e){
   let ui = new UI();
   ui.deleteBook(e.target);
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent); 
   e.preventDefault();
 });
